d7ebf32b7a7a0d6b013f81c4b7f2182f5f93f952
Shubhi Mishra <shubhimishra053@gmail.com>
2021-10-07 19:53:24 +0530

    Changes

--- src/app/tabs/connectors/connectors.component.ts
+++ src/app/tabs/connectors/connectors.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { logo } from 'src/assets/images/logo.svg';
+declare var require: any;
 // import { Tab } from '../tab.model';
 @Component({
   selector: 'app-connectors',
@@ -6,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connectors.component.css'],
 })
 export class ConnectorsComponent implements OnInit {
+  require : any; 
   constructor() {}
   
   // logo = require('src/assets/images/logo.svg').default;
+  logos = new URL('src/assets/images/logo.svg', import.meta.url)
   ngOnInit(): void {}
   source: { title: string; value: number; description: string }[] = [
     { title: 'Appjs', value: 200, description: 'Available' },
@@ -17,6 +21,7 @@ export class ConnectorsComponent implements OnInit {
     { title: 'Appjs', value: 200, description: 'Available' },
     { title: 'AWS', value: 2005, description: 'Available' },
     { title: 'branch', value: 3000, description: 'Available' },
+    
   ];
 
   destination: { title: string; value: number; description: string }[] = [
@@ -26,5 +31,9 @@ export class ConnectorsComponent implements OnInit {
     { title: 'Appjs', value: 200, description: 'Available' },
     { title: 'AWS', value: 2005, description: 'Available' },
     { title: 'branch', value: 3000, description: 'Available' },
+    { title: 'AWS', value: 2005, description: 'Available' },
+    
+    
+   
   ];
 }
