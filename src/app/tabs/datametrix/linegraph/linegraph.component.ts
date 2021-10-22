3e62b520bd2281adce8198ee42f2f70d9817c7fb
Shubhi Mishra <shubhimishra053@gmail.com>
2021-10-22 23:41:00 +0530

    Css

--- src/app/tabs/datametrix/linegraph/linegraph.component.ts
+++ src/app/tabs/datametrix/linegraph/linegraph.component.ts
@@ -1,93 +1,107 @@
-import { Component, OnInit } from '@angular/core';
-import * as moment from 'moment';
-import { FormBuilder } from "@angular/forms";
-// import * as Highcharts from 'highcharts';
-import * as Highcharts from 'highcharts/highstock';
-// import * as HighchartsMore from 'highcharts/highcharts-more';
-import * as HighchartsExporting from 'highcharts/modules/exporting';
 
-// HighchartsMore(Highcharts);
-// HighchartsExporting(Highcharts);
-declare var require: any;
+import { Component, OnInit } from "@angular/core";
+import * as Highcharts from "highcharts";
+import * as moment from 'moment';
+ import { FormBuilder } from "@angular/forms";
+import * as HighchartsMore from "highcharts/highcharts-more";
+import * as HighchartsExporting from "highcharts/modules/exporting";
+ declare var require: any;
 const More = require('highcharts/highcharts-more');
 More(Highcharts);
 
-// const Exporting = require('highcharts/modules/exporting');
-// Exporting(Highcharts);
-
-// const ExportData = require('highcharts/modules/export-data');
-// ExportData(Highcharts);
-
-// const Accessibility = require('highcharts/modules/accessibility');
-// Accessibility(Highcharts);
+//  const Exporting = require('highcharts/modules/exporting');
+//  Exporting(Highcharts);
+// HighchartsMore(Highcharts);
+// HighchartsExporting(Highcharts);
 
 @Component({
   selector: 'app-linegraph',
-  templateUrl: './linegraph.component.html',
-  styleUrls: ['./linegraph.component.css'],
+    templateUrl: './linegraph.component.html',
+   styleUrls: ['./linegraph.component.css'],
 })
 export class LinegraphComponent implements OnInit {
+  
   selected: any = { startDate: moment, endDate: moment };
-  seriesData = [2, 4, 3, 8, 7, 12];
-  tempData = [2, 5, 8, 4, 6, 5];
-  temp = false;
-  kebab = '../../assets/images/kebab.svg';
+    seriesData = [2, 4, 3, 8, 7, 12];
+    tempData = [2, 5, 8, 4, 6, 5];
+    temp = false;
+    kebab = '../../assets/images/kebab.svg';
+  
+    timePeriod : any = ['Yearly', 'Monthly', 'Weekly']
+   
 
-  timePeriod : any = ['Yearly', 'Monthly', 'Weekly']
-  // onInputChng(){
-  //   this.seriesData = this.tempData;
-  //   console.log(this.seriesData);
-  // }
-  constructor(public fb: FormBuilder) {}
-
-
-   timeForm = this.fb.group({
-     name : ['']
-   })
-
-   options: any = {
-    chart: {},
-    title: {
-      text: null,
-    },
-    button: {
-      enabled: true,
+  chart: any;
+  updateFlag = false;
+  Highcharts = Highcharts;
+  chartConstructor = "chart";
+  chartCallback: any;
+  
+  chartOptions: any = {
+    
+   title: {
+     text: null,
+   },
+    series: [
+      {
+        data: [1, 2, 3, 6, 9,8]
+      }
+    ],
+    exporting: {
+      enabled: true
     },
     xAxis: {
-      title: {
-        text: 'Date',
-      },
-      categories: ['Jan20', 'Feb20', 'Mar20', 'Apr20', 'May20', 'Jun20'],
-    },
+            title: {
+              text: 'Date',
+            },
+            categories: ['Jan20', 'Feb20', 'Mar20', 'Apr20', 'May20', 'Jun20'],
+          },
     yAxis: {
+      allowDecimals: false,
       title: {
-        text: 'Data',
-      },
-    },
-    series: [
-      { 
-        data: this.seriesData,
-        type: 'line',
-      },
-    ],
+        text: "Data"
+      }
+    }
   };
-onInputChng(){
-  
-  // this.options.addSeries(this.tempData,false)
-  
-  
-  
- 
-}
-  sminDate: any;
 
-  ngOnInit(): void {
-    Highcharts.chart('container', this.options);
-    const current = new Date();
-    this.sminDate = {
-      year: current.getFullYear(),
-      month: current.getMonth() + 1,
-      day: current.getDate(),
+  constructor(public fb: FormBuilder) {
+    const self = this;
+
+    this.chartCallback = (chart: any) => {
+      
+      self.chart = chart;
     };
+  }
+  timeForm = this.fb.group({
+         name : ['']
+       })
+  ngOnInit() {}
+
+  updateChart() {
+    console.log("kii");
+    const self = this,
+      chart = this.chart;
+
+    chart.showLoading();
+    setTimeout(() => {
+      chart.hideLoading();
+      let rend = Math.floor(Math.random()*10+1);
+      console.log(rend);
+      let temp = [10, 25, 20, 6];
+      temp.push(rend);
+      let rend1 = Math.floor(Math.random()*10+1);
+      temp.push(rend1);
+      self.chartOptions.series = [
+        {
+          data: temp
+        },
+        
+      ];
+
+      // self.chartOptions.title = {
+      //   text: "Updated title!"
+      // };
+
+      self.updateFlag = true;
+    }, 2000);
   }
 }
--- src/app/tabs/datametrix/linegraphs/linegraphs.component.ts
+++ src/app/tabs/datametrix/linegraphs/linegraphs.component.ts
@@ -1,86 +1,109 @@
-import { Component, OnInit } from '@angular/core';
-import * as moment from 'moment';
-import { FormBuilder } from "@angular/forms";
-// import * as Highcharts from 'highcharts';
-import * as Highcharts from 'highcharts/highstock';
-import * as HighchartsMore from 'highcharts/highcharts-more';
-import * as HighchartsExporting from 'highcharts/modules/exporting';
 
-// HighchartsMore(Highcharts);
-// HighchartsExporting(Highcharts);
-declare var require: any;
+import { Component, OnInit } from "@angular/core";
+import * as Highcharts from "highcharts";
+import * as moment from 'moment';
+ import { FormBuilder } from "@angular/forms";
+import * as HighchartsMore from "highcharts/highcharts-more";
+import * as HighchartsExporting from "highcharts/modules/exporting";
+ declare var require: any;
 const More = require('highcharts/highcharts-more');
 More(Highcharts);
 
-// const Exporting = require('highcharts/modules/exporting');
-// Exporting(Highcharts);
-
-// const ExportData = require('highcharts/modules/export-data');
-// ExportData(Highcharts);
-
-// const Accessibility = require('highcharts/modules/accessibility');
-// Accessibility(Highcharts);
-
+//  const Exporting = require('highcharts/modules/exporting');
+//  Exporting(Highcharts);
+// HighchartsMore(Highcharts);
+// HighchartsExporting(Highcharts);
 
 @Component({
   selector: 'app-linegraphs',
-  templateUrl: './linegraphs.component.html',
-  styleUrls: ['./linegraphs.component.css']
+templateUrl: './linegraphs.component.html',
+styleUrls: ['./linegraphs.component.css']
 })
 export class LinegraphsComponent implements OnInit {
-
+  
   selected: any = { startDate: moment, endDate: moment };
-  seriesData = [2, 4, 3, 8, 7, 12];
-  tempData = [2, 5, 8, 4, 6, 5];
+    seriesData = [2, 4, 3, 8, 7, 12];
+    tempData = [2, 5, 8, 4, 6, 5];
+    temp = false;
+    kebab = '../../assets/images/kebab.svg';
+  
+    timePeriod : any = ['Yearly', 'Monthly', 'Weekly']
+   
 
-  onInputChng(){
-    this.seriesData = this.tempData;
-    console.log("hi");
-  }
-  kebab = '../../assets/images/kebab.svg';
-  timePeriod : any = ['Yearly', 'Monthly', 'Weekly']
-  constructor(public fb: FormBuilder) {}
-  timeForm = this.fb.group({
-    name : ['']
-  })
-
-
-  public options: any = {
-    chart: {},
-    title: {
-      text: null,
-    },
-    button: {
-      enabled: true,
-    },
-    xAxis: {
-      title: {
-        text: 'Date',
-      },
-      categories: ['Jan20', 'Feb20', 'Mar20', 'Apr20', 'May20', 'Jun20'],
-    },
-    yAxis: {
-      title: {
-        text: 'Data',
-      },
-    },
+  chart: any;
+  updateFlag = false;
+  Highcharts = Highcharts;
+  chartConstructor = "chart";
+  chartCallback: any;
+  
+  chartOptions: any = {
+    
+   title: {
+     text: null,
+   },
     series: [
       {
-        data: this.seriesData,
-        type: 'line',
-      },
+        data: [1, 2, 3, 6, 9,8]
+      }
     ],
+    exporting: {
+      enabled: true
+    },
+    xAxis: {
+            title: {
+              text: 'Date',
+            },
+            categories: ['Jan20', 'Feb20', 'Mar20', 'Apr20', 'May20', 'Jun20'],
+          },
+    yAxis: {
+      allowDecimals: false,
+      title: {
+        text: "Data"
+      }
+    }
   };
 
-  sminDate: any;
+  constructor(public fb: FormBuilder) {
+    const self = this;
 
-  ngOnInit(): void {
-    Highcharts.chart('containerSc', this.options);
-    const current = new Date();
-    this.sminDate = {
-      year: current.getFullYear(),
-      month: current.getMonth() + 1,
-      day: current.getDate(),
+    this.chartCallback = (chart: any) => {
+      
+      self.chart = chart;
     };
   }
+  timeForm = this.fb.group({
+         name : ['']
+       })
+  ngOnInit() {}
+
+  updateChart() {
+    console.log("kii");
+    const self = this,
+      chart = this.chart;
+
+    chart.showLoading();
+    setTimeout(() => {
+      chart.hideLoading();
+      let rend = Math.floor(Math.random()*10+1);
+      console.log(rend);
+      let temp = [10, 25, 20, 6];
+      temp.push(rend);
+      let rend1 = Math.floor(Math.random()*10+1);
+      temp.push(rend1);
+      self.chartOptions.series = [
+        {
+          data: temp
+        },
+        
+      ];
+
+      // self.chartOptions.title = {
+      //   text: "Updated title!"
+      // };
+
+      self.updateFlag = true;
+    }, 2000);
+  }
 }
+
+
